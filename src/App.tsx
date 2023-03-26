c7d7f88acccf9d86805309125423afdee58645a5
Abhishek Raj <abhishekr@dewsolutions.in>
2023-03-26 20:15:39 +0530

    Numeric clientId

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import ScanDevice from "./pages/scanDevices/index"
 import TestPage from './pages/test-page';
 import NotFound from './pages/not-found';
 import { useDeviceStatus,useSocketConnected} from './labhub/status';
-import { assertClientId } from './labhub/utils';
 import styles from './styles/App.module.css';
 import { GrTest } from '@react-icons/all-files/gr/GrTest';
 import FunctionSelection from './pages/functionProcedure/FunctionSelection';
@@ -37,12 +36,6 @@ import InsertionAbsorbanceMeasuring from './components/MeasureAbsorbance/Inserti
 function App() {
   const [status] = useDeviceStatus();
   const [connected] = useSocketConnected();
-  useEffect(() => {
-    const clientId = assertClientId();
-    if (!clientId) {
-      console.error('Could not set clientId in localStorage');
-    }
-  }, []);
 
   return (
     <div className={styles.app}>
--- src/labhub/utils.ts
+++ src/labhub/utils.ts
@@ -1,19 +1,58 @@
-import short from 'short-uuid';
 import { deviceStatus } from './status';
 import { LABHUB_CLIENT_ID } from '../utils/const';
 import { ClientType } from '../types/common';
 
-export const getClientId = () => {
-  return localStorage.getItem(LABHUB_CLIENT_ID);
+export const getClientId = (): string | null => {
+  const clientIdx = localStorage.getItem(LABHUB_CLIENT_ID);
+  let clientId = null;
+  if (clientIdx) {
+    clientId = Number.parseInt(clientIdx, 10) || null; // NOTE: 0, NaN -> null
+    if (!clientId) {
+      console.error('[ERROR] Invalid clientId encountered in localStorage! [1]');
+      return null;
+    }
+  }
+
+  if (clientId) {
+    // Validate client ID
+    const uint16Array = Uint16Array.of(clientId);
+    if (clientId !== uint16Array[0]) {
+      console.error('[ERROR] Invalid clientId encountered in localStorage! [2]');
+      return null;
+    }  
+  }
+  return clientId === null ? clientId : `${clientId}`;
 };
 
-export const assertClientId = () => {
+const setClientId = (clientId?: number): string | null => {
+  if (clientId === undefined) {
+    // Generate a new client ID
+    const uint16 = Math.floor(Math.random() * (2 ** 16 - 1)) + 1; // 1-65535
+    const uint16Array = Uint16Array.of(uint16);
+    clientId = uint16Array[0];  
+  } else {
+    // Validate client ID
+    const uint16Array = Uint16Array.of(clientId);
+    if (clientId !== uint16Array[0]) {
+      console.error('[ERROR] Invalid clientId passed!');
+      return null;
+    }
+  }
+
+  localStorage.setItem(LABHUB_CLIENT_ID, `${clientId}`);
+  const retVal = getClientId();
+  if (!retVal) {
+    console.error('Error retrieving set clientId from localStorage:', clientId);
+  }
+  return retVal;
+};
+
+export const assertClientId = (): string | null => {
   let clientId = getClientId();
   if (!clientId) {
-    clientId = short.generate();
-    localStorage.setItem(LABHUB_CLIENT_ID, clientId);
+    clientId = setClientId();
   }
-  return getClientId();
+  return clientId;
 };
 
 export const getClientType =  (): ClientType => {

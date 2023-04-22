2fc710614b00fefef6de2e68b3432e0490849339
Abhishek Raj <abhishekr@dewsolutions.in>
2023-04-22 14:24:28 +0530

    Create generic getValueHook

--- src/labhub/status.ts
+++ src/labhub/status.ts
@@ -2,52 +2,37 @@ import { useEffect, useState } from 'react';
 import { BehaviorSubject } from 'rxjs';
 import { DeviceStatus, DeviceStatusUpdate, DeviceDataFeed, DeviceDataFeedUpdate, ClientChannelRequest, ClientChannelResponse } from '../types/common';
 
-export const connectionAttemptOngoing = new BehaviorSubject<boolean>(false);
-export const applicationErrorMessage = new BehaviorSubject<string | null>(null);
-export const deviceConnected = new BehaviorSubject<boolean>(false);
-export const deviceStatus = new BehaviorSubject<DeviceStatus | null>(null);
 export const deviceStatusUpdate = new BehaviorSubject<DeviceStatusUpdate | null>(null);
 export const deviceDataFeedUpdate = new BehaviorSubject<DeviceDataFeedUpdate | null>(null);
 export const clientChannelResponse = new BehaviorSubject<ClientChannelResponse | null>(null);
 export const clientChannelRequest = new BehaviorSubject<ClientChannelRequest | null>(null);
+
+export const connectionAttemptOngoing = new BehaviorSubject<boolean>(false);
+export const applicationErrorMessage = new BehaviorSubject<string | null>(null);
+export const deviceConnected = new BehaviorSubject<boolean>(false);
+export const deviceStatus = new BehaviorSubject<DeviceStatus | null>(null);
 export const deviceDataFeed = new BehaviorSubject<DeviceDataFeed>({
   sensor: null,
   heater: null,
   rgb: null,
 });
 
-export const useDeviceConnected = () => {
-  const [connected, setConnected] = useState(deviceConnected.value);
+const getValueHook = <T>(behaviorSubject: BehaviorSubject<any>) => () => {
+  const [value, setValue] = useState(behaviorSubject.value);
 
   useEffect(() => {
-    const subs = deviceConnected.subscribe((value) => setConnected(value));
+    const subs = behaviorSubject.subscribe((value) => setValue(value));
     return () => subs.unsubscribe();
   }, []);
 
-  return [connected];
+  return [value as T];
 };
 
-export const useDeviceStatus = () => {
-  const [status, setStatus] = useState(deviceStatus.value);
-
-  useEffect(() => {
-    const subs = deviceStatus.subscribe(value => setStatus(value === null ? null : {...value}));
-    return () => subs.unsubscribe();
-  }, []);
-
-  return [status];
-};
-
-export const useDeviceDataFeed = () => {
-  const [data, setData] = useState(deviceDataFeed.value);
-
-  useEffect(() => {
-    const subs = deviceDataFeed.subscribe(value => setData({...value}));
-    return () => subs.unsubscribe();
-  }, []);
-
-  return [data];
-};
+export const useDeviceConnected = getValueHook<boolean>(deviceConnected);
+export const useDeviceStatus = getValueHook<DeviceStatus | null>(deviceStatus);
+export const useDeviceDataFeed = getValueHook<DeviceDataFeed>(deviceDataFeed);
+export const useConnectionStablished = getValueHook<boolean>(connectionAttemptOngoing);
+export const useErrorMessage = getValueHook<string | null>(applicationErrorMessage);
 
 export const getDeviceApiResponse = (): DeviceApiResponse => {
   return {
@@ -56,26 +41,6 @@ export const getDeviceApiResponse = (): DeviceApiResponse => {
   };
 };
 
-export const useConnectionStablished = () => {
-  const [connected, setConnected] = useState(connectionAttemptOngoing.value);
-
-  useEffect(() => {
-    const subs = connectionAttemptOngoing.subscribe((value) => setConnected(value));
-    return () => subs.unsubscribe();
-  }, []);
-
-  return [connected];
-};
-export const useErrorMessage = () => {
-  const [errorMessage, setErrorMessage] = useState(applicationErrorMessage.value);
-
-  useEffect(() => {
-    const subs = applicationErrorMessage.subscribe((value) => setErrorMessage(value));
-    return () => subs.unsubscribe();
-  }, []);
-
-  return [errorMessage];
-};
 export interface DeviceApiResponse {
   version: string;
   forceUpdate: boolean;

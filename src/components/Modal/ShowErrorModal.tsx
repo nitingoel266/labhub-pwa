9ecc0379c5e1391df79e6800612643f249ad897f
deveshps <deveshps@dewsolutions.in>
2023-04-24 18:20:45 +0530

    change style for error modal acc to message type

--- src/components/Modal/ShowErrorModal.tsx
+++ src/components/Modal/ShowErrorModal.tsx
@@ -7,13 +7,16 @@ const ShowErrorModal = () => {
     const [appMessage] = useAppMessage();
 
     const errorMessage = appMessage?.message ?? null;
-
+    const messageType = appMessage?.type;
+    const getMessageType = {"warn":"Warning","info":"Information","error":"Error Message"}
+    
     const setErrorMessage = (value:any) => {
         applicationMessage.next(value)
     }
     const handleResetConnection = () => {
         setErrorMessage(null)
     }
+    const messageColor = {backgroundColor:messageType === 'warn' ? "#FFC000" : messageType === "info" ? "#424C58" : "#CC0000"}
     return errorMessage ? <div style={{position:"absolute",zIndex:1}}>
     {errorMessage &&
     <div
@@ -23,9 +26,9 @@ const ShowErrorModal = () => {
     className={styles.SecondWrapper}
     >
         <div className={styles.TextContainer}>
-                <div className={styles.ErrorHeadertext}>
+                <div className={styles.ErrorHeadertext} style={messageColor}>
                     <img src={WhiteWarningIcon} style={{width:20,marginRight:10}} alt="warning icon"/>
-                    <div>Error Message</div>
+                    <div>{messageType && getMessageType[messageType]}</div>
                 </div>
                 <div className={styles.ErrorBodyWrapper}>
                     <div className={styles.Bodytext}>

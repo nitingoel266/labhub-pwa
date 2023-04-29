85d3d796e9ae75678df02739566964c89a010ba7
Abhishek Raj <abhishekr@dewsolutions.in>
2023-04-29 18:31:45 +0530

    Update init-setup

--- src/labhub/init-setup.ts
+++ src/labhub/init-setup.ts
@@ -1,10 +1,8 @@
-import { initSetup as initSetupMock, uninitSetup as uninitSetupMock } from "./setup";
+import { uninitSetup as uninitSetupMock } from "./setup";
 import { applicationMessage } from "./status";
 
 export const initSetup = async (): Promise<boolean> => {
-  if (!{}) {
-    return await initSetupMock();
-  }
+  // return await initSetupMock();
   applicationMessage.next({ type: 'info', message: 'Awesome! Your deploy is complete.' });
   return true;
 };
